10f30a7209d86411bb0a03b326c93c662372d14c
devofy <artemijv65@gmail.com>
2024-10-30 22:53:16 +0500

    Complete NAV and Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { Navigation } from './components/Navigation';
+import { Header } from './components/Header';
+
 function App() {
-  return <>Hello world!</>;
+  return (
+    <div className='wrapper'>
+      <Navigation />
+      <Header />
+    </div>
+  );
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/components/**/*.tsx', 'index.html', './src/pages/**/*.tsx'],
+  content: ['./src/components/**/*.tsx', 'index.html', './src/*.tsx'],
+  theme: {
+    extend: {
+      gridTemplateRows: {
+        headerBlocks: '280px 280px',
+      },
+      colors: {
+        'text-color': '#32344F',
+        blue: '#39BEF4',
+        'white-blue': '#75CEF6',
+        'dark-blue': '#0772B4',
+      },
+      textColor: {
+        DEFAULT: '#32344F',
+      },
+    },
+  },
   plugins: [],
 };
