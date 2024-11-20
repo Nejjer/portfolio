1a0e7a5ce64d685dfbe21b42486f80a7f979256d
devofy <artemijv65@gmail.com>
2024-11-21 00:02:08 +0500

    Сверстал мобилку

--- src/components/Contacts/Contacts.tsx
+++ src/components/Contacts/Contacts.tsx
@@ -2,25 +2,38 @@ import { FC } from 'react';
 import { Input } from '../Input';
 import { Textarea } from '../Textarea';
 import { Button } from '../Button';
+import Phone from './Phone.svg?react';
+import Telegram from './Telegram.svg?react';
+import Mail from './Mail.svg?react';
 
 export const Contacts: FC = () => {
   const contact = (text: string) => (
     <li className={'mb-6 flex w-full items-center gap-4'}>
-      <div className={'bg-blue h-6 w-6'}></div> {text}
+      <div className={'h-6 w-6 bg-blue'}></div> {text}
     </li>
   );
 
   return (
-    <section className={'mb-11 flex gap-10'}>
-      <div className={'grid basis-[760px] grid-cols-1 grid-rows-7 gap-3'}>
-        <h4 className={'text-3xl font-bold'}>Связаться напрямую</h4>
-        <Input />
-        <Input />
-        <Textarea className={'row-span-3'} />
-        <Button className={'justify-self-end'}>Отправить</Button>
+    <section
+      className={
+        'bg-ultra-white-blue mb-0 flex flex-col gap-10 md:mb-11 md:flex-row md:bg-none'
+      }
+    >
+      <div
+        className={
+          'flex grid-cols-1 grid-rows-7 flex-col gap-3 md:grid md:basis-[760px]'
+        }
+      >
+        <h4 className={'mt-5 text-lg font-bold md:mt-0 md:text-3xl'}>
+          Связаться напрямую
+        </h4>
+        <Input placeholder={'Имя'} />
+        <Input placeholder={'E-mail'} />
+        <Textarea placeholder={'Ваше сообщение'} className={'row-span-3'} />
+        <Button className={'md:justify-self-end'}>Отправить</Button>
       </div>
 
-      <div className={'h-full w-[360px] flex-1'}>
+      <div className={'hidden h-full w-[360px] flex-1 md:block'}>
         <h4 className={'mb-8 text-3xl font-bold '}>Прочие контакты</h4>
         <ul className={'bg-grey1 p-8'}>
           {contact('Контакт 1')}
@@ -28,6 +41,17 @@ export const Contacts: FC = () => {
           {contact('Контакт 1')}
           {contact('Контакт 1')}
         </ul>
+      </div>
+
+      <div>
+        <h6 className={'font-bold text-dark-blue'}>
+          Малыгина Наталья Владимировна
+        </h6>
+        <div className={'mb-5 mt-2 flex gap-2.5'}>
+          <Phone />
+          <Telegram />
+          <Mail />
+        </div>
       </div>
     </section>
   );
--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom';
-import { FC } from 'react';
+import { FC, useState } from 'react';
+import Burger from './Burger.svg?react';
 
 interface Props {
   isAbsolutePos?: boolean;
@@ -7,29 +8,59 @@ interface Props {
 
 export const Navigation: FC<Props> = ({ isAbsolutePos }) => {
   const className = isAbsolutePos ? 'nav-pos' : '';
+  const [active, setActive] = useState(false);
+
+  window.onclick = () => setActive(false);
 
   return (
-    <div
-      className={
-        'hidden items-center justify-between bg-white px-8 md:flex ' + className
-      }
-    >
-      <Link to={'/'}>
-        <h3 className={'text-2xl font-bold'}>Заголовок</h3>
-      </Link>
-      <nav>
-        <ul className={'flex justify-between gap-4'}>
-          <Link to={'/aboutme'} className={'cursor-pointer p-4'}>
-            Обо мне
-          </Link>
-          <Link to={'/portfolio'} className={'cursor-pointer p-4'}>
-            Портфолио
-          </Link>
-          <Link to={'contacts'} className={'cursor-pointer p-4'}>
-            Контакты
-          </Link>
-        </ul>
-      </nav>
-    </div>
+    <>
+      <div
+        className={
+          'hidden items-center justify-between bg-white px-8 md:flex ' +
+          className
+        }
+      >
+        <Link to={'/'}>
+          <h3 className={'text-2xl font-bold'}>Заголовок</h3>
+        </Link>
+        <nav>
+          <ul className={'flex justify-between gap-4'}>
+            <Link to={'/aboutme'} className={'cursor-pointer p-4'}>
+              Обо мне
+            </Link>
+            <Link to={'/portfolio'} className={'cursor-pointer p-4'}>
+              Портфолио
+            </Link>
+            <Link to={'contacts'} className={'cursor-pointer p-4'}>
+              Контакты
+            </Link>
+          </ul>
+        </nav>
+      </div>
+      <div
+        className={`bg-white-transparent absolute right-5 top-5 flex ${active ? 'h-auto' : 'h-10'} ${active ? 'w-36' : 'w-10'} cursor-pointer items-center justify-center rounded-[20px] transition-all md:hidden`}
+      >
+        <Burger
+          onClick={(e) => {
+            e.stopPropagation();
+            setActive((active) => !active);
+          }}
+          className={active ? 'hidden' : 'block'}
+        />
+        <nav className={active ? 'block' : 'hidden'}>
+          <ul className={'flex flex-col justify-between gap-2'}>
+            <Link to={'/aboutme'} className={'cursor-pointer p-2'}>
+              Обо мне
+            </Link>
+            <Link to={'/portfolio'} className={'cursor-pointer px-2'}>
+              Портфолио
+            </Link>
+            <Link to={'contacts'} className={'cursor-pointer p-2'}>
+              Контакты
+            </Link>
+          </ul>
+        </nav>
+      </div>
+    </>
   );
 };
