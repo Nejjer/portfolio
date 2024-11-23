095d411405bc47b785119558e9e319878bfa1e1e
devofy <artemijv65@gmail.com>
2024-11-23 21:09:24 +0500

    Мобильный Navigation

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,7 +16,7 @@ export default {
         blue: '#39BEF4',
         'white-blue': '#75CEF6',
         'ultra-white-blue': 'rgba(203, 240, 255, 1)',
-        'white-transparent': 'rgba(255, 255, 255, 0.7)',
+        'white-transparent': 'rgb(171,228,250)',
         'dark-blue': '#0772B4',
         grey1: '#EBF9FF',
         grey4: '#9EBECB',
