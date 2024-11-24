6cffa0f79737aee373cef1dc091eed4953e80a11
devofy <artemijv65@gmail.com>
2024-11-24 14:14:51 +0500

    Начал присоединять бекенд

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,19 @@ import { Route, Routes } from 'react-router-dom';
 import { Main } from './pages/Main';
 import { Portfolio } from './pages/Portfolio';
 import { AboutMe } from './pages/AboutMe';
+import { WithStore } from './stores/WithStore.tsx';
 
 function App() {
   return (
-    <div className='wrapper '>
-      <Routes>
-        <Route path='/' element={<Main />} />
-        <Route path='/portfolio' element={<Portfolio />} />
-        <Route path='/aboutme' element={<AboutMe />} />
-      </Routes>
-    </div>
+    <WithStore>
+      <div className='wrapper '>
+        <Routes>
+          <Route path='/' element={<Main />} />
+          <Route path='/portfolio' element={<Portfolio />} />
+          <Route path='/aboutme' element={<AboutMe />} />
+        </Routes>
+      </div>
+    </WithStore>
   );
 }
 
--- src/pages/Portfolio/Publications/Publications.tsx
+++ src/pages/Portfolio/Publications/Publications.tsx
@@ -1,4 +1,6 @@
-import { FC } from 'react';
+import { FC, useContext } from 'react';
+import { AppStoreContext, StoreCtx } from '../../../stores/WithStore.tsx';
+import { observer } from 'mobx-react';
 
 interface IPublication {
   text: string;
@@ -10,33 +12,40 @@ const PublicationItem = ({ text }: IPublication) => (
   </li>
 );
 
-const publicationItems: IPublication[] = [
-  {
-    text: 'Дикий северный олень (Rangifer tarandus L.) Восточного Таймыра: особенности пространственного размещения',
-  },
-  {
-    text: 'Принцип социальной справедливости в использовании ресурсов дикого северного оленя (Rangifer tarandus L.) как основа жизнеобеспечения аборигенного населения Крайнего Севера',
-  },
-  {
-    text: 'Поведенческие характеристики дикого северного оленя (Rangifer tarandus L.) в период охоты в контексте стратегии и механизмов рационального природопользования',
-  },
-  {
-    text: 'Хоросинхронная динамика диких северных оленей (Rangifer tarandus L.) на территории Восточного Таймыра как отклик на внешние вызовы',
-  },
-  {
-    text: 'XIII Международная конференция «Размышления о науке в “темные” времена»',
-  },
-];
+// const publicationItems: IPublication[] = [
+//   {
+//     text: 'Дикий северный олень (Rangifer tarandus L.) Восточного Таймыра: особенности пространственного размещения',
+//   },
+//   {
+//     text: 'Принцип социальной справедливости в использовании ресурсов дикого северного оленя (Rangifer tarandus L.) как основа жизнеобеспечения аборигенного населения Крайнего Севера',
+//   },
+//   {
+//     text: 'Поведенческие характеристики дикого северного оленя (Rangifer tarandus L.) в период охоты в контексте стратегии и механизмов рационального природопользования',
+//   },
+//   {
+//     text: 'Хоросинхронная динамика диких северных оленей (Rangifer tarandus L.) на территории Восточного Таймыра как отклик на внешние вызовы',
+//   },
+//   {
+//     text: 'XIII Международная конференция «Размышления о науке в “темные” времена»',
+//   },
+// ];
 
-export const Publications: FC = () => {
+const Publications: FC = () => {
+  const {
+    appStore: { mainStore },
+  } = useContext<AppStoreContext>(StoreCtx);
+
   return (
     <section className={'mt-20 h-[351px]'}>
       <h4 className={'mb-10 text-3xl font-bold'}>Публикации</h4>
       <ul className={'grid columns-1 gap-0.5 bg-blue'}>
-        {publicationItems.map(({ text }) => (
-          <PublicationItem text={text} />
+        {mainStore.publications.map(({ title }) => (
+          <PublicationItem text={title} />
         ))}
       </ul>
     </section>
   );
 };
+
+const connected = observer(Publications);
+export { connected as Publications };
