e52b6e48a206f9f4dee2b6909ecb19ec723f515f
devofy <artemijv65@gmail.com>
2024-11-27 10:22:49 +0500

    Some fixes

--- client/admin/src/pages/Portfolio/Portfolio.tsx
+++ client/admin/src/pages/Portfolio/Portfolio.tsx
@@ -1,8 +1,16 @@
 import { FC, useContext, useEffect } from 'react';
 import { Link, useParams } from 'react-router-dom';
+import { Form } from 'react-final-form';
+
 import { Card } from '@gravity-ui/uikit';
 import { observer } from 'mobx-react';
 import { AppStoreContext, StoreCtx } from '../../stores/WithStore.tsx';
+import {
+  dynamicConfig,
+  DynamicField,
+  Spec,
+  SpecTypes,
+} from '@gravity-ui/dynamic-forms';
 
 const Item: FC<{ name: string; path: string }> = ({ name, path }) => {
   return (
@@ -25,6 +33,75 @@ const sections = [
   { path: 'conferences', name: 'Конференции' },
 ];
 
+const specs: Spec[] = [
+  {
+    type: SpecTypes.String,
+    viewSpec: {
+      type: 'base',
+      layout: 'row',
+      layoutTitle: 'ФИО',
+      placeholder: 'ФИО',
+    },
+  },
+  {
+    type: SpecTypes.String,
+    viewSpec: {
+      type: 'textarea',
+      layout: 'row',
+      layoutTitle: 'Короткая информация',
+      placeholder: 'Короткая информация',
+    },
+  },
+  {
+    type: SpecTypes.String,
+    viewSpec: {
+      type: 'base',
+      layout: 'row',
+      layoutTitle: 'Слоган',
+      placeholder: 'Слоган',
+    },
+  },
+  {
+    type: SpecTypes.Object,
+    properties: {
+      phone: {
+        type: SpecTypes.String,
+        viewSpec: {
+          type: 'base',
+          layout: 'row',
+          placeholder: 'Телефон',
+          layoutTitle: 'Телефон',
+        },
+      },
+      telegram: {
+        type: SpecTypes.String,
+        pattern: 's*s',
+        viewSpec: {
+          type: 'base',
+          layout: 'row',
+          placeholder: 'Telegram',
+          layoutTitle: 'Telegram',
+        },
+      },
+      email: {
+        type: SpecTypes.String,
+        viewSpec: {
+          type: 'base',
+          layout: 'row',
+          placeholder: 'Почта',
+          layoutTitle: 'Почта',
+        },
+      },
+    },
+    viewSpec: {
+      type: 'base',
+      layout: 'accordeon',
+      layoutTitle: 'Контакты',
+      layoutOpen: true,
+    },
+  },
+];
+
 const Portfolio: FC = () => {
   const { id } = useParams();
 
@@ -37,10 +114,19 @@ const Portfolio: FC = () => {
   }, []);
 
   return (
-    <div className={'flex gap-10 p-10'}>
-      {sections.map((section) => (
-        <Item key={section.path} name={section.name} path={section.path} />
-      ))}
+    <div>
+      <div className={'flex gap-10 p-10'}>
+        {sections.map((section) => (
+          <Item key={section.path} name={section.name} path={section.path} />
+        ))}
+      </div>
+      <Form onSubmit={() => console.log('')}>
+        {() =>
+          specs.map((spec) => (
+            <DynamicField spec={spec} name={'name'} config={dynamicConfig} />
+          ))
+        }
+      </Form>
     </div>
   );
 };
